a9fff2eff3544b3e7e56203ac2f2904d155defff
Sparsh Agarwal <sparshaga25@gmail.com>
2022-10-31 11:15:59 +0530

    Cleaned App Directory

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  static String id = "homepage";
+
+  const Homepage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text("Hello World!"),
+      ),
+    );
+  }
+}
