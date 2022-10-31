71e5d3f76135a56124dac3f930c51887786f599f
Sparsh Agarwal <sparshaga25@gmail.com>
2022-10-31 14:26:04 +0530

    Created custom Appbar

---
+++ lib/components/appbar/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+AppBar customAppBar({
+  required BuildContext context,
+  required String title,
+  List<Widget>? actions,
+}) {
+  return AppBar(
+    title: Text(title),
+    centerTitle: true,
+    elevation: 0,
+    systemOverlayStyle: SystemUiOverlayStyle(
+      statusBarColor: Theme.of(context).primaryColor,
+      statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
+      statusBarBrightness: Brightness.light, // For iOS (dark icons)
+    ),
+    actions: actions,
+  );
+}
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multisig_wallet_with_delegation/components/appbar/custom_app_bar.dart';
 
 class Homepage extends StatefulWidget {
   static String id = "homepage";
@@ -15,11 +16,32 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      appBar: customAppBar(
+        context: context,
+        title: widget.title,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Icon(
+              Icons.account_circle,
+              size: 32,
+            ),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text("Hello World!"),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(
+          20,
+          (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
