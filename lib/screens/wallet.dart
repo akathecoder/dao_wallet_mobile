724621466e1f7b89c7c020efe2ffd15cee3aafe4
Sparsh Agarwal <sparshaga25@gmail.com>
2022-11-07 20:54:19 +0530

    Added Glass Effect to Wallet Page

--- lib/screens/wallet.dart
+++ lib/screens/wallet.dart
@@ -1,6 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:multisig_wallet_with_delegation/components/appbar/custom_app_bar.dart';
-import 'package:multisig_wallet_with_delegation/components/general/glass_box.dart';
+import 'package:flutter/services.dart';
 
 class WalletArguments {
   const WalletArguments({required this.address, required this.title});
@@ -31,38 +32,79 @@ class _WalletState extends State<Wallet> {
     final args = ModalRoute.of(context)!.settings.arguments as WalletArguments;
 
     return Scaffold(
-      appBar: customAppBar(
-        context: context,
-        title: args.title,
-      ),
-      bottomNavigationBar: GlassBox(
-        child: BottomNavigationBar(
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey[300],
-          backgroundColor: Colors.transparent,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 0,
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentBottonIndex,
-          onTap: _handleIndexChanged,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: "Label 1"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.access_alarm_rounded), label: "Label 2"),
-          ],
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(56.0),
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: AppBar(
+              backgroundColor: Colors.black.withOpacity(0.2),
+              title: Text(args.title),
+              centerTitle: true,
+              elevation: 0.0,
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness:
+                    Brightness.dark, // For Android (dark icons)
+                statusBarBrightness: Brightness.light, // For iOS (dark icons)
+              ),
+            ),
+          ),
         ),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              tileColor: Colors.red,
-              title: Text("Loda"),
+      bottomNavigationBar: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: BottomNavigationBar(
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.grey[300],
+            backgroundColor: Colors.black.withOpacity(0.2),
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentBottonIndex,
+            onTap: _handleIndexChanged,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.notifications), label: "Label 1"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.access_alarm_rounded), label: "Label 2"),
+            ],
+          ),
+        ),
+      ),
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 106,
             ),
-          );
-        },
+          ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 1 / 1.5,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    tileColor: Colors.red,
+                    title: Text("Loda"),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+          const SliverPadding(
+            padding: EdgeInsets.only(bottom: 80.0),
+          ),
+        ],
       ),
     );
   }
