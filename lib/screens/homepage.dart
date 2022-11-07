7243b1ac8eab9aadf20cc2a4d7bece09825b89a8
Sparsh Agarwal <sparshaga25@gmail.com>
2022-11-07 10:43:10 +0530

    Created Wallet Grid

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multisig_wallet_with_delegation/components/appbar/custom_app_bar.dart';
+import 'package:multisig_wallet_with_delegation/components/blockies/custom_blockie.dart';
 import 'package:multisig_wallet_with_delegation/components/homepage/profile_info_app_bar_bottom.dart';
 
 class Homepage extends StatefulWidget {
@@ -13,25 +14,39 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-enum Layout {
-  grid,
-  list,
-}
-
 class _HomepageState extends State<Homepage> {
-  Layout walletLayout = Layout.grid;
-
-  void swapWalletlayout() {
-    if (walletLayout == Layout.grid) {
-      setState(() {
-        walletLayout = Layout.list;
-      });
-    } else {
-      setState(() {
-        walletLayout = Layout.grid;
-      });
-    }
-  }
+  List<Widget> wallets = [
+    const WalletCard(
+      color: Colors.blue,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+    const WalletCard(
+      color: Colors.red,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+    const WalletCard(
+      color: Colors.purple,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+    const WalletCard(
+      color: Colors.brown,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+    const WalletCard(
+      color: Colors.blue,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+    const WalletCard(
+      color: Colors.red,
+      address: "0xb5f7E8dD674E8fEaA37E7817AD26dA72adadce1f",
+      name: "Super Secret Wallet",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -52,45 +67,76 @@ class _HomepageState extends State<Homepage> {
         bottom: profileInfoAppBarBottom(context: context),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 4.0),
-                child: IconButton(
-                  onPressed: () {
-                    swapWalletlayout();
-                  },
-                  icon: Icon(
-                    walletLayout == Layout.grid
-                        ? Icons.view_list_rounded
-                        : Icons.grid_view_rounded,
-                    size: 32,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Text(
+              "Wallets",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: List.generate(
-                20,
-                (index) {
-                  return Center(
-                    child: Text(
-                      'Item $index',
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
-                  );
-                },
+            child: GridView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return wallets[index];
+              },
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1 / 1.5,
               ),
+              itemCount: wallets.length,
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class WalletCard extends StatelessWidget {
+  const WalletCard({
+    Key? key,
+    required this.color,
+    required this.name,
+    required this.address,
+  }) : super(key: key);
+
+  final MaterialColor color;
+  final String name;
+  final String address;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: color[50],
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CustomBlockie(size: 150, data: address),
+            Column(
+              children: [
+                Text(
+                  address,
+                  style: Theme.of(context).textTheme.bodySmall,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 2),
+                Text(
+                  name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
