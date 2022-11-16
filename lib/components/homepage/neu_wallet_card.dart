b135872215aba2f96850a6ea9656f8d1a3348eba
Sparsh Agarwal <sparshaga25@gmail.com>
2022-11-16 17:19:29 +0530

    Created Neu Wallet Card

--- lib/components/homepage/neu_wallet_card.dart
+++ lib/components/homepage/neu_wallet_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:multisig_wallet_with_delegation/components/blockies/custom_blockie.dart';
 import 'package:multisig_wallet_with_delegation/components/general/neu_box.dart';
+import 'package:multisig_wallet_with_delegation/screens/wallet.dart';
 
 class WalletCard extends StatelessWidget {
   const WalletCard({super.key, required this.name, required this.address});
@@ -11,10 +13,45 @@ class WalletCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(14.0),
-      child: NeuBox(
-          child: Column(
-        children: [],
-      )),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            Wallet.id,
+            arguments: WalletArguments(title: name, address: address),
+          );
+        },
+        child: NeuBox(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: CustomBlockie(size: 120, data: address),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Column(
+                children: [
+                  Text(
+                    address,
+                    style: Theme.of(context).textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 2),
+                  Text(
+                    name,
+                    style: Theme.of(context).textTheme.titleLarge,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
