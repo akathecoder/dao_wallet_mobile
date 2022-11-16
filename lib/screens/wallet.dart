c344bc7dbead8c3555fa4204dd230fc7bf5012eb
Sparsh Agarwal <sparshaga25@gmail.com>
2022-11-16 10:39:20 +0530

    made wallet page

--- lib/screens/wallet.dart
+++ lib/screens/wallet.dart
@@ -1,6 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:multisig_wallet_with_delegation/components/wallet/wallet_appbar.dart';
-import 'package:multisig_wallet_with_delegation/components/wallet/wallet_bottom_navigation_bar.dart';
+import 'package:flutter/services.dart';
 
 class WalletArguments {
   const WalletArguments({required this.address, required this.title});
@@ -30,47 +31,112 @@ class _WalletState extends State<Wallet> {
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as WalletArguments;
 
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: walletAppbar(args),
-        bottomNavigationBar: walletBottomNavigationBar(
-          currentIndex: _currentBottonIndex,
-          onTap: _handleIndexChanged,
-        ),
-        body: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            const SliverToBoxAdapter(
-              child: SizedBox(
-                height: (106 + 56),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                pinned: true,
+                backgroundColor: Colors.black.withOpacity(0.2),
+                title: Text(args.title),
+                centerTitle: true,
+                elevation: 0.0,
+                systemOverlayStyle: const SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  statusBarIconBrightness:
+                      Brightness.dark, // For Android (dark icons)
+                  statusBarBrightness: Brightness.light, // For iOS (dark icons)
+                ),
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(56.0),
+                  child: TabBar(
+                    tabs: const [
+                      Tab(
+                        text: "Crypto",
+                        icon: Icon(Icons.currency_bitcoin),
+                      ),
+                      Tab(
+                        text: "NFTs",
+                        icon: Icon(Icons.filter_frames),
+                      ),
+                    ],
+                    indicatorWeight: 4,
+                    indicatorColor: Colors.black.withOpacity(0.25),
+                  ),
+                ),
               ),
-            ),
-            SliverGrid(
+            ];
+          },
+          body: TabBarView(children: [
+            GridView.builder(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
                 childAspectRatio: 1 / 1.5,
               ),
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      tileColor: Colors.red,
-                      title: Text("Item ${index + 1}"),
-                    ),
-                  );
-                },
-                childCount: 20,
-              ),
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text("Hello"),
+                    tileColor: Colors.amber,
+                  ),
+                );
+              },
             ),
-            const SliverPadding(
-              padding: EdgeInsets.only(bottom: 80.0),
-            ),
-          ],
+          ]),
         ),
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   final args = ModalRoute.of(context)!.settings.arguments as WalletArguments;
+
+  //   return DefaultTabController(
+  //     length: 2,
+  //     child: Scaffold(
+  //       extendBodyBehindAppBar: true,
+  //       appBar: walletAppbar(args),
+  //       bottomNavigationBar:  (
+  //         currentIndex: _currentBottonIndex,
+  //         onTap: _handleIndexChanged,
+  //       ),
+  //       body: CustomScrollView(
+  //         physics: const BouncingScrollPhysics(),
+  //         slivers: [
+  //           const SliverToBoxAdapter(
+  //             child: SizedBox(
+  //               height: (106 + 56),
+  //             ),
+  //           ),
+  //           SliverGrid(
+  //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+  //               crossAxisCount: 3,
+  //               childAspectRatio: 1 / 1.5,
+  //             ),
+  //             delegate: SliverChildBuilderDelegate(
+  //               (context, index) {
+  //                 return Padding(
+  //                   padding: const EdgeInsets.all(8.0),
+  //                   child: ListTile(
+  //                     tileColor: Colors.red,
+  //                     title: Text("Item ${index + 1}"),
+  //                   ),
+  //                 );
+  //               },
+  //               childCount: 20,
+  //             ),
+  //           ),
+  //           const SliverPadding(
+  //             padding: EdgeInsets.only(bottom: 80.0),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
